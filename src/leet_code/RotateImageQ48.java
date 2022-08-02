8e7de8fdaf0f2a76286705e8ffa94e73791ccc4c
SagunDevkota <sagundevyt@gmail.com>
2022-08-02 15:22:19 +0545

    Day 62

---
+++ src/leet_code/RotateImageQ48.java
@@ -0,0 +1,31 @@
+package leet_code;
+
+public class RotateImageQ48 {
+
+	public static void main(String[] args) {
+		RotateImageQ48 rotateImageQ48 = new RotateImageQ48();
+		rotateImageQ48.rotate(new int[][] {{1,2,3},{4,5,6},{7,8,9}});
+	}
+	public void rotate(int[][] matrix) {
+        for (int i = 0;i<matrix.length;i++){
+            for(int j = i;j<matrix.length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0;i<matrix.length;i++){
+            for(int j = 0;j<matrix.length/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][(matrix.length-1)-j];
+                matrix[i][(matrix.length-1)-j] = temp;
+            }
+        }
+         for (int i = 0;i<matrix.length;i++){
+             for(int j = 0;j<matrix.length;j++){
+                 System.out.print(matrix[i][j]+" ");
+             }
+             System.out.println();
+         }
+    }
+}
